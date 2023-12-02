499d071e2801e3c129ad46ada852d08b16d0af3e
arthurducept <arthur.ducept@gmail.com>
2023-12-02 20:59:44 +0100

    Update file with localized date format :rocket:

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const fs = require('fs');
-const path = './README.md';
 
-// Fonction pour obtenir la date du jour sous la forme "Lun 23 Octobre 2023"
-function getFormattedDate() {
-  const days = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
-  const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+const LOCALIZATION = {
+  fr: {
+    days: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+    months: [
+      'Janvier',
+      'Février',
+      'Mars',
+      'Avril',
+      'Mai',
+      'Juin',
+      'Juillet',
+      'Août',
+      'Septembre',
+      'Octobre',
+      'Novembre',
+      'Décembre',
+    ],
+  },
+  en: {
+    days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    months: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+  },
+};
 
+function main() {
+  updateFile('./README.md', 'fr');
+  updateFile('./README-EN.md', 'en');
+}
+
+// Fonction pour obtenir la date du jour sous la forme 'Lun 23 Octobre 2023'
+function getFormattedDate(days, months) {
   const date = new Date();
   date.setHours(date.getHours() + 2);
   const day = days[date.getDay()];
@@ -16,17 +54,24 @@ function getFormattedDate() {
   return `${day} ${dayOfMonth} ${month} ${year}`;
 }
 
-// Lecture du fichier
-fs.readFile(path, 'utf8', function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
+function updateFile(filePath, lang) {
+  // Lecture du fichier
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
 
-  // Remplacement de la ligne "_Last Updated: ..."
-  const result = data.replace(/_Last Updated: .+?_/, `_Last Updated: ${getFormattedDate()}_`);
+    const months = LOCALIZATION[lang].months;
+    const days = LOCALIZATION[lang].days;
 
-  // Écriture du nouveau contenu dans le fichier
-  fs.writeFile(path, result, 'utf8', function(err) {
-    if (err) return console.log(err);
+    // Remplacement de la ligne '_Last Updated: ...'
+    const result = data.replace(/_Last Updated: .+?_/, `_Last Updated: ${getFormattedDate(days, months)}_`);
+
+    // Écriture du nouveau contenu dans le fichier
+    fs.writeFile(filePath, result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
   });
-});
+}
+
+main();
